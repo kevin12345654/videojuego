03106b5531530139b90d86a96f324ab54fa1bd3b
kevin12345654 <miranda_kevin.02@hotmail.com>
2018-09-17 19:53:47 -0500

    atributos

---
+++ Videojuego/src/models/NewClass.java
@@ -0,0 +1,51 @@
+package models;
+
+/**
+ *
+ * @author kevin
+ */
+    class videojuego {
+    
+        //Atributos
+        public String genero;
+        public String costo;
+        public String clasificacion;
+        public String graficos;
+        public String consola;
+        
+        
+        
+        //Metodos
+        
+        
+        public void genero(){
+            
+            System.out.println("Escribe Genero");
+        }
+
+
+        public void costo(){
+            
+            System.out.println("Escribe Costo");
+        }
+        
+        public void clasificacion(){
+            
+            System.out.println("Escribe La Clasificacion");
+            
+        }
+
+        public void graficos(){
+            
+            System.out.println("Escribe Tipo De Graficos");
+        }
+        
+        public void consola(){
+            
+            System.out.println("Escribe Tipo De Consola");
+        }
+
+
+
+}
+                
